36d44ea54623e00f1a99709485c304b253557bf0
Theo <theo.piedade@gmail.com>
2023-11-05 22:12:11 -0300

    Second Commit

---
+++ src/routers/enrollments-router.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { authenticateToken, validateBody } from '@/middlewares';
+import { getEnrollmentByUser, postCreateOrUpdateEnrollment, getAddressFromCEP } from '@/controllers';
+import { createOrUpdateEnrollmentSchema } from '@/schemas';
+
+const enrollmentsRouter = Router();
+
+enrollmentsRouter
+  .get('/enrollments/cep')
+  .get('/cep', getAddressFromCEP)
+  .all('/*', authenticateToken)
+  .get('/', getEnrollmentByUser)
+  .post('/', validateBody(createOrUpdateEnrollmentSchema), postCreateOrUpdateEnrollment);
+
+export { enrollmentsRouter };
